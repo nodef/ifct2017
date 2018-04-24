6fbd5600007962497a8b6027d1cccef6e826822f
Subhajit Sahu <wolfram77@gmail.com>
2018-04-24 15:00:06 +0530

    :snowman: return boolean AND results only

--- build.js
+++ build.js
@@ -1,43 +1,17 @@
-const nutrients = require('@ifct2017/nutrients');
-const natural = require('natural');
 const csv = require('csv');
-const path = require('path');
 const fs = require('fs');
+const os = require('os');
 
-function matchOr(arr, vals) {
-  for(var val of vals)
-    if(arr.includes(val)) return true;
-  return false;
-};
-
-function matchAnd(arr, vals) {
-  for(var val of vals)
-    if(!arr.includes(val)) return false;
-  return true;
-};
-
-const DESC = new Map();
-var dir = nutrients();
-var nams = fs.readdirSync(dir);
-for(var nam of nams) {
-  var fil = path.join(dir, nam);
-  var txt = fs.readFileSync(fil, 'utf8');
-  DESC.set(nam.replace('.txt', ''), txt);
-}
-
-var z = 'code,name,tags,desc\n';
-var srm = fs.createReadStream('columns.csv').pipe(csv.parse({columns: true, comment: '#'}));
-srm.on('data', (r) => {
-  var txt = r.code+' '+r.name+' '+r.tags;
-  txt = txt.replace(/[.,;\s\(\)\-\:\*]+/g, ' ').trim(); r.desc = r.desc||'';
-  var src = txt.split(' ').map((v) => natural.PorterStemmer.stem(v.toLowerCase()));
-  for(var [k, v] of DESC) {
-    var or = k.includes('+');
-    var tgt = k.split(or? '+':'-').map((v) => natural.PorterStemmer.stem(v.toLowerCase()));
-    if(or? matchOr(src, tgt):matchAnd(src, tgt)) r.desc = v.replace(/\r?\n/g, '\\n');
-  }
-  z += `"${r.code}","${r.name}","${r.tags}","${r.desc}"\n`;
+var map = new Map();
+var stream = fs.createReadStream('index.csv').pipe(csv.parse({columns: true, comment: '#'}));
+stream.on('data', (r) => {
+  map.set(r.code, {code: r.code, name: r.name, tags: r.tags});
 });
-srm.on('end', () => {
-  fs.writeFileSync('index.csv', z);
+stream.on('end', () => {
+  var z = `const CORPUS = new Map([${os.EOL}`;
+  for(var [k, v] of map)
+    z += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],${os.EOL}`;
+  z += `]);${os.EOL}`;
+  z += `module.exports = CORPUS;${os.EOL}`;
+  fs.writeFileSync('corpus.js', z);
 });
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+const lunr = require('lunr');
 const path = require('path');
-function columns() {
+const corpus = require('./corpus');
+
+var index = lunr(function() {
+  this.ref('code');
+  this.field('code');
+  this.field('name');
+  this.field('tags');
+  this.pipeline.remove(lunr.stopWordFilter);
+  for(var r of corpus.values())
+    this.add(r);
+});
+
+function csv() {
   return path.join(__dirname, 'index.csv');
 };
+
+function columns(txt) {
+  var z = [], txt = txt.replace(/\W/g, ' ');
+  var mats = index.search(txt), max = 0;
+  for(var mat of mats)
+    max = Math.max(max, Object.keys(mat.matchData.metadata).length);
+  for(var mat of mats)
+    if(Object.keys(mat.matchData.metadata).length===max) z.push(corpus.get(mat.ref));
+  return z;
+};
+columns.csv = csv;
+columns.corpus = corpus;
 module.exports = columns;
