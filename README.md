f4de17b410596861b20f16bd73885cf064b4bef7
Subhajit Sahu <wolfram77@gmail.com>
2018-05-11 14:56:10 +0530

    :banana: repo add

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# compositingcentres
-Regional compositing centres and sample size of each region in Indian Food Composition Tables 2017.
+# @ifct2017/compositingcentres
+
+[![IFCT2017](http://ninindia.org/images/ifct_2017.png)](http://ninindia.org/ifct_2017.htm)
+
+Regional compositing centres and sample size of each region in [Indian Food Composition Tables 2017].
+
+```javascript
+const compositingCentres = require('@ifct2017/compositingcentres');
+// compositingCentres()
+// -> <path to compositing centres csv>
+```
+
+
+[Indian Food Composition Tables 2017]: http://ifct2017.com/
---
+++ index.js
@@ -0,0 +1,27 @@
+const lunr = require('lunr');
+const corpus = require('./corpus');
+const path = require('path');
+
+var index = lunr(function() {
+  this.ref('region');
+  this.field('region');
+  this.field('centre');
+  for(var r of corpus.values())
+    this.add(r);
+});
+
+function csv() {
+  return path.join(__dirname, 'index.csv');
+};
+function compositingCentres(txt) {
+  var z = [], txt = txt.replace(/\W/g, ' ');
+  var mats = index.search(txt), max = 0;
+  for(var mat of mats)
+    max = Math.max(max, Object.keys(mat.matchData.metadata).length);
+  for(var mat of mats)
+    if(Object.keys(mat.matchData.metadata).length===max) z.push(corpus.get(mat.ref));
+  return z;
+};
+compositingCentres.csv = csv;
+compositingCentres.corpus = corpus;
+module.exports = compositingCentres;
