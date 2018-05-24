7e24a4821be90f191a0b508dd0d8de4e3c877406
Subhajit Sahu <wolfram77@gmail.com>
2018-05-24 18:04:52 +0530

    :snowman: index.js: add .sql(), pk=analyte

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const columns = require('@ifct2017/columns');
+const Sql = require('sql-extra');
 const corpus = require('./corpus');
 const path = require('path');
 
@@ -6,10 +7,16 @@ function csv() {
   return path.join(__dirname, 'index.csv');
 };
 
+function sql(tab='methods', opt={}) {
+  return Sql.setupTable(tab, {analyte: 'TEXT', method: 'TEXT', reference: 'TEXT'}, corpus.values(),
+    Object.assign({pk: 'analyte', index: true, tsvector: {analyte: 'A', method: 'B', reference: 'C'}}, opt));
+};
+
 function methods(txt) {
   var cs = columns(txt);
   return cs.length>0? corpus.get(cs[0].code):null;
 };
 methods.csv = csv;
+methods.sql = sql;
 methods.corpus = corpus;
 module.exports = methods;
