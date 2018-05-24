b760d57bc5066bf1c15d05c080ae95b7cb032ba5
Subhajit Sahu <wolfram77@gmail.com>
2018-05-24 08:04:23 +0530

    :snowman: index.js: add .sql(), pk=districts

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const Sql = require('sql-extra');
 const corpus = require('./corpus');
 const path = require('path');
 
 function csv() {
   return path.join(__dirname, 'index.csv');
 };
+
+function sql(tab='frequencydistribution', opt={}) {
+  return Sql.setupTable(tab, {districts: 'TEXT', states: 'INT', selected: 'INT', sampled: 'INT'},
+    corpus.values(), {pk: 'districts', index: true, tsvector: {districts: 'A'}});
+};
+
 function frequencyDistribution(dis) {
   if(dis<=5) return corpus.get(1);
   if(dis<=10) return corpus.get(6);
@@ -11,5 +18,6 @@ function frequencyDistribution(dis) {
   return corpus.get(Math.floor((dis-1)/10)*10+1);
 };
 frequencyDistribution.csv = csv;
+frequencyDistribution.sql = sql;
 frequencyDistribution.corpus = corpus;
 module.exports = frequencyDistribution;
