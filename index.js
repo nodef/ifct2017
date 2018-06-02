c77a7a9a977fddd314ae0c5ecddf65418776e566
Subhajit Sahu <wolfram77@gmail.com>
2018-06-02 13:39:31 +0530

    :snowman: index: .load(), and .sql() async

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
+const Sql = require('sql-extra');
+const parse = require('csv-parse');
 const lunr = require('lunr');
-const csvParse = require('csv-parse/lib/sync');
 const path = require('path');
 const fs = require('fs');
 
-function corpus() {
-  var z = new Map();
-  var text = fs.readFileSync('index.csv', 'utf8');
-  var data = csvParse(text, {columns: true, comment: '#'});
-  for(var r of data)
-    z.set(r.name, r);
-  return z;
-};
-const CORPUS = corpus();
-
-var index = lunr(function() {
-  this.ref('key');
-  this.field('name');
-  this.field('code');
-  this.pipeline.remove(lunr.stopWordFilter);
-  for(var r of CORPUS.values())
-    this.add({key: r.name, name: r.name.replace(/\W+/g, ' '), code: r.code});
-});
+const corpus = new Map();
+var index = null;
+var ready = null;
 
 function csv() {
   return path.join(__dirname, 'index.csv');
 };
+
+async function sql(tab, opt={}) {
+  var cols = {name: 'TEXT', code: 'TEXT'};
+  var opt = Object.assign({pk: 'name', index: true, tsvector: {name: 'A', code: 'B'}}, opt);
+  var stream = fs.createReadStream(csv()).pipe(parse({columns: true, comment: '#'}));
+  var z = Sql.createTable(tab, cols, opt);
+  z = await Sql.insertInto.stream(tab, stream, opt, z);
+  z = Sql.setupTable.index(tab, cols, opt, z);
+  return z;
+};
+
+function loadCorpus() {
+  return new Promise((fres) => {
+    var stream = fs.createReadStream(csv()).pipe(parse({columns: true, comment: '#'}));
+    stream.on('data', (r) => corpus.set(r.name, r));
+    stream.on('end', fres);
+  });
+};
+
+function setupIndex() {
+  index = lunr(function() {
+    this.ref('key');
+    this.field('name');
+    this.field('code');
+    this.pipeline.remove(lunr.stopWordFilter);
+    for(var r of CORPUS.values())
+      this.add({key: r.name, name: r.name.replace(/\W+/g, ' '), code: r.code});
+  });
+};
+
+function load() {
+  ready = ready||loadCorpus();
+  return ready.then(setupIndex);
+};
+
 function codes(txt) {
+  if(index==null) return [];
   var z = [], txt = txt.replace(/\W/g, ' ');
   var mats = index.search(txt), max = 0;
   for(var mat of mats)
@@ -35,5 +57,7 @@ function codes(txt) {
   return z;
 };
 codes.csv = csv;
-codes.corpus = CORPUS;
+codes.sql = sql;
+codes.load = load;
+codes.corpus = corpus;
 module.exports = codes;
