4bf4f36f04994041d581b0e117118c9cde668adf
Subhajit Sahu <wolfram77@gmail.com>
2018-06-05 15:14:36 +0530

    :snowman: corpus load when needed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const columns = require('@ifct2017/columns');
 const Sql = require('sql-extra');
-const corpus = require('./corpus');
 const path = require('path');
+
+var corpus = new Map();
+var ready = false;
+
 
 function csv() {
   return path.join(__dirname, 'index.csv');
@@ -14,11 +17,20 @@ function sql(tab='methods', opt={}) {
     Object.assign({pk: 'analyte', index: true, tsvector: {analyte: 'A', method: 'B', reference: 'C'}}, opt));
 };
 
+function load() {
+  if(ready) return true;
+  for(var [k, v] of require('./corpus'))
+    corpus.set(k, v);
+  return ready = true;
+};
+
 function methods(txt) {
+  if(!ready) return null;
   var cs = columns(txt);
   return cs.length>0? corpus.get(cs[0].code):null;
 };
 methods.csv = csv;
 methods.sql = sql;
+methods.load = load;
 methods.corpus = corpus;
 module.exports = methods;
