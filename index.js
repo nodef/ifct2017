cfcdda9e867162c4e4c702ef7318adbac3b0b37f
Subhajit Sahu <wolfram77@gmail.com>
2018-06-05 13:59:25 +0530

    :snowman: should load corpus first

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const Sql = require('sql-extra');
 const lunr = require('lunr');
-const corpus = require('./corpus');
 const path = require('path');
 
-var index = lunr(function() {
-  this.ref('component');
-  this.field('component');
-  for(var r of corpus.values())
-    this.add(r);
-});
+var corpus = new Map();
+var index = null;
+var ready = false;
+
+
+function loadCorpus() {
+  for(var [k, v] of require('./corpus'))
+    corpus.set(k, v);
+};
+
+function setupIndex() {
+  index = lunr(function() {
+    this.ref('component');
+    this.field('component');
+    for(var r of corpus.values())
+      this.add(r);
+  });
+};
 
 function csv() {
   return path.join(__dirname, 'index.csv');
@@ -19,7 +30,14 @@ function sql(tab='energies', opt={}) {
     Object.assign({pk: 'component', index: true, tsvector: {component: 'A'}}, opt));
 };
 
+function load() {
+  if(ready) return true;
+  loadCorpus(); setupIndex();
+  return ready = true;
+};
+
 function energies(txt) {
+  if(index==null) return [];
   var z = [], txt = txt.replace(/\W/g, ' ');
   var mats = index.search(txt), max = 0;
   for(var mat of mats)
@@ -30,5 +48,6 @@ function energies(txt) {
 };
 energies.csv = csv;
 energies.sql = sql;
+energies.load = load;
 energies.corpus = corpus;
 module.exports = energies;
