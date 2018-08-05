c56d28907dbb6360242b8648ec859b05526969e6
Subhajit Sahu <wolfram77@gmail.com>
2018-08-05 16:38:15 +0530

    :snowman: ancestry should include parents too

--- build.js
+++ build.js
@@ -13,22 +13,17 @@ function readAsset() {
     stream.on('data', r => {
       var {code, parents} = r;
       parents = parents? new Set(parents.split(' ')):new Set();
-      asset.set(code, {code, parents});
+      asset.set(code, {parents});
     });
     stream.on('end', () => fres(asset));
   });
 };
 
-function getLineage(cod, z) {
+function getAncestry(cod, z=new Set()) {
   var par = asset.get(cod).parents;
-  if(par.size===0) return z.add(cod);
+  if(par.size===0) return z;
   for(p of par)
-    getLineage(p, z);
-  return z;
-};
-function getAncestry(cod) {
-  var z = getLineage(cod, new Set());
-  z.delete(cod);
+    getAncestry(p, z.add(p));
   return z;
 };
 function getChildren(cod) {
@@ -40,27 +35,27 @@ function getChildren(cod) {
 
 function updateAsset() {
   var z = new Map();
-  for(var r of asset.values()) {
-    var {code, parents} = r;
+  for(var [code, r] of asset) {
+    var {parents} = r;
     parents = Array.from(parents).join(' ');
     var ancestry = Array.from(getAncestry(code)).join(' ');
     var children = Array.from(getChildren(code)).join(' ');
-    z.set(code, {code, parents, ancestry, children});
+    z.set(code, {parents, ancestry, children});
   }
   asset = z;
 };
 
 function writeIndex() {
   var z = `code,parents,ancestry,children${os.EOL}`;
-  for(var {code, parents, ancestry, children} of asset.values())
-    z += `${code},${parents},${ancestry},${children}${os.EOL}`;
+  for(var [code, r] of asset)
+    z += `${code},${r.parents},${r.ancestry},${r.children}${os.EOL}`;
   fs.writeFileSync('index.csv', z);
 };
 
 function writeCorpus() {
   var z = `var CORPUS = new Map([${os.EOL}`;
-  for(var [k, v] of asset)
-    z += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],${os.EOL}`;
+  for(var [code, r] of asset)
+    z += `  ["${code}", ${JSON.stringify(r).replace(/\"(\w+)\":/g, '$1:')}],${os.EOL}`;
   z += `]);${os.EOL}`;
   z += `module.exports = CORPUS;${os.EOL}`;
   fs.writeFileSync('corpus.js', z);
