75432734a5b6c033b26a4e008e83903c26e0b97e
Subhajit Sahu <wolfram77@gmail.com>
2018-08-05 15:46:38 +0530

    :snowman: update build script (no factor, parents)

--- build.js
+++ build.js
@@ -1,24 +1,15 @@
-const csv = require('csv');
+const parse = require('csv-parse');
 const fs = require('fs');
 const os = require('os');
 
 var map = new Map();
-var stream = fs.createReadStream('index.csv').pipe(csv.parse({columns: true, comment: '#'}));
-stream.on('data', (r) => {
-  var {code, name, factor, tops, tags} = r;
-  factor = parseInt(factor);
-  map.set(code, {code, name, factor, tops, tags});
-});
+var stream = fs.createReadStream('index.csv').pipe(parse({columns: true, comment: '#'}));
+stream.on('data', r => map.set(r.code, r));
 stream.on('end', () => {
-  var z = `(function(root) {${os.EOL}`;
-  z += `var CORPUS = new Map([${os.EOL}`;
+  var z = `var CORPUS = new Map([${os.EOL}`;
   for(var [k, v] of map)
     z += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],${os.EOL}`;
   z += `]);${os.EOL}`;
-  z += `if(typeof module!=='undefined' && module.exports) return module.exports = CORPUS;${os.EOL}`;
-  z += `if(typeof root.ifct2017==='undefined') root.ifct2017 = {};${os.EOL}`;
-  z += `if(typeof root.ifct2017.columns==='undefined') root.ifct2017.columns = {};${os.EOL}`;
-  z += `root.ifct2017.columns.corpus = CORPUS;${os.EOL}`;
-  z += `})(this);${os.EOL}`;
+  z += `module.exports = CORPUS;${os.EOL}`;
   fs.writeFileSync('corpus.js', z);
 });
