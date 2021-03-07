25093aaa692e0d6203289470474bc8b29ec9859a
Subhajit Sahu <wolfram77@gmail.com>
2021-03-07 09:15:52 +0530

    :ringed_planet: modernize build, index

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
-const Sql = require('sql-extra');
-const lunr = require('lunr');
 const path = require('path');
+const lunr = require('lunr');
+const esql = require('sql-extra');
 
-var corpus = new Map();
+var corpus = null;
 var index = null;
-var ready = false;
 
-function loadCorpus() {
-  for(var [k, v] of require('./corpus'))
-    corpus.set(k, v);
-};
 
-function setupIndex() {
-  index = lunr(function() {
+
+
+function createIndex() {
+  return lunr(function() {
     this.ref('region');
     this.field('region');
     this.field('states');
-    for(var r of corpus.values())
+    for (var r of corpus.values())
       this.add({region: r.region, states: r.states.replace(/\W/g, ' ')});
   });
-};
+}
+
+function load() {
+  if (corpus) return corpus;
+  corpus = require('./corpus');
+  index = createIndex();
+  return corpus;
+}
 
 function csv() {
   return path.join(__dirname, 'index.csv');
-};
+}
 
 function sql(tab='regions', opt={}) {
-  return Sql.setupTable(tab, {region: 'TEXT', states: 'TEXT'}, require('./corpus').values(),
+  return esql.setupTable(tab, {region: 'TEXT', states: 'TEXT'}, require('./corpus').values(),
     Object.assign({pk: 'region', index: true, tsvector: {region: 'A', states: 'B'}}, opt));
-};
+}
 
-function load() {
-  if(ready) return true;
-  loadCorpus(); setupIndex();
-  return ready = true;
-};
 
 function regions(txt) {
-  if(index==null) return [];
-  var z = [], txt = txt.replace(/\W/g, ' ');
-  var mats = index.search(txt), max = 0;
-  for(var mat of mats)
-    max = Math.max(max, Object.keys(mat.matchData.metadata).length);
-  for(var mat of mats)
-    if(Object.keys(mat.matchData.metadata).length===max) z.push(corpus.get(mat.ref));
-  return z;
+  if (!corpus) load();
+  var a = [], txt = txt.replace(/\W/g, ' ');
+  var ms = index.search(txt), max = 0;
+  for(var m of ms)
+    max = Math.max(max, Object.keys(m.matchData.metadata).length);
+  for(var m of ms)
+    if(Object.keys(m.matchData.metadata).length===max) a.push(corpus.get(m.ref));
+  return a;
 };
+regions.load = load;
 regions.csv = csv;
 regions.sql = sql;
-regions.load = load;
-regions.corpus = corpus;
 module.exports = regions;
