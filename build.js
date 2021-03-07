9bf9e67fb3bf0b2cd1ab0ecd07dbbfc60defaffb
Subhajit Sahu <wolfram77@gmail.com>
2021-03-07 06:56:19 +0530

    :ringed_planet: modernify build, index

--- build.js
+++ build.js
@@ -4,18 +4,29 @@ const os = require('os');
 
 var map = new Map();
 var stream = fs.createReadStream('index.csv').pipe(csv.parse({columns: true, comment: '#'}));
+
+
+
+
+function writeFile(pth, d) {
+  d = d.replace(/\r?\n/g, os.EOL);
+  fs.writeFileSync(pth, d);
+}
+
+
 stream.on('data', (r) => {
   r.states = parseFloat(r.states);
   r.selected = parseFloat(r.selected);
   r.sampled = parseFloat(r.sampled);
   var key = parseFloat(r.districts.split('-')[0].replace(/\D/g, ''));
-  map.set(key===70? 71:key, r);
+  map.set(key===70? 71 : key, r);
 });
+
 stream.on('end', () => {
-  var z = `const CORPUS = new Map([${os.EOL}`;
-  for(var [k, v] of map)
-    z += `  [${k}, ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],${os.EOL}`;
-  z += `]);${os.EOL}`;
-  z += `module.exports = CORPUS;${os.EOL}`;
-  fs.writeFileSync('corpus.js', z);
+  var a = `const CORPUS = new Map([\n`;
+  for (var [k, v] of map)
+    a += `  [${k}, ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],\n`;
+  a += `]);\n`;
+  a += `module.exports = CORPUS;\n`;
+  writeFile('corpus.js', a);
 });
--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const Sql = require('sql-extra');
 const path = require('path');
+const esql = require('sql-extra');
 
-var corpus = new Map();
-var ready = false;
+var corpus = null;
 
+
+
+
+function load() {
+  if (corpus) return corpus;
+  corpus = require('./corpus')
+  return corpus;
+}
 
 function csv() {
   return path.join(__dirname, 'index.csv');
-};
+}
 
 function sql(tab='frequencydistribution', opt={}) {
-  return Sql.setupTable(tab, {districts: 'TEXT', states: 'INT', selected: 'INT', sampled: 'INT'},
+  return esql.setupTable(tab, {districts: 'TEXT', states: 'INT', selected: 'INT', sampled: 'INT'},
     require('./corpus').values(), Object.assign({pk: 'districts', index: true, tsvector: {districts: 'A'}}, opt));
-};
-
-function load() {
-  if(ready) return true;
-  for(var [k, v] of require('./corpus'))
-    corpus.set(k, v);
-  return ready = true;
-};
+}
 
 function frequencyDistribution(dis) {
-  if(!ready) return null;
-  if(dis<=5) return corpus.get(1);
-  if(dis<=10) return corpus.get(6);
-  if(dis>70) return corpus.get(71);
+  if (!corpus) return null;
+  if (dis <= 5) return corpus.get(1);
+  if (dis <= 10) return corpus.get(6);
+  if (dis > 70) return corpus.get(71);
   return corpus.get(Math.floor((dis-1)/10)*10+1);
-};
+}
 frequencyDistribution.csv = csv;
 frequencyDistribution.sql = sql;
 frequencyDistribution.load = load;
-frequencyDistribution.corpus = corpus;
 module.exports = frequencyDistribution;
