18cab495523420aa3a4aee9a7f6d0d4bf3ccc28a
Subhajit Sahu <wolfram77@gmail.com>
2021-03-07 08:32:13 +0530

    :ringed_planet: modernize build, index

--- build.js
+++ build.js
@@ -4,16 +4,27 @@ const os = require('os');
 
 var map = new Map();
 var stream = fs.createReadStream('index.csv').pipe(csv.parse({columns: true, comment: '#'}));
-stream.on('data', (r) => {
+
+
+
+
+function writeFile(pth, d) {
+  d = d.replace(/\r?\n/g, os.EOL);
+  fs.writeFileSync(pth, d);
+}
+
+
+stream.on('data', r => {
   r.districts = parseInt(r.districts, 10);
   r.selected = parseInt(r.selected, 10);
   map.set(r.sno, r);
 });
+
 stream.on('end', () => {
-  var z = `var CORPUS = new Map([${os.EOL}`;
-  for(var [k, v] of map)
-    z += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],${os.EOL}`;
-  z += `]);${os.EOL}`;
-  z += `module.exports = CORPUS;${os.EOL}`;
-  fs.writeFileSync('corpus.js', z);
+  var a = `var CORPUS = new Map([\n`;
+  for (var [k, v] of map)
+    a += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:')}],\n`;
+  a += `]);\n`;
+  a += `module.exports = CORPUS;\n`;
+  writeFile('corpus.js', a);
 });
--- index.js
+++ index.js
@@ -1,55 +1,53 @@
-const Sql = require('sql-extra');
-const lunr = require('lunr');
 const path = require('path');
+const lunr = require('lunr');
+const esql = require('sql-extra');
 
-var corpus = new Map();
+var corpus = null;
 var index = null;
 var ready = false;
 
 
-function loadCorpus() {
-  for(var [k, v] of require('./corpus'))
-    corpus.set(k, v);
-};
 
-function setupIndex() {
-  index = lunr(function() {
+
+function createIndex() {
+  return lunr(function() {
     this.ref('sno');
     this.field('sno');
     this.field('state');
     this.pipeline.remove(lunr.stopWordFilter);
-    for(var r of corpus.values())
+    for (var r of corpus.values())
       this.add(r);
   });
-};
+}
+
+function load() {
+  if (corpus) return corpus;
+  corpus = require('./corpus');
+  index = createIndex();
+  return corpus;
+}
 
 function csv() {
   return path.join(__dirname, 'index.csv');
-};
+}
 
 function sql(tab='samplingunits', opt={}) {
-  return Sql.setupTable(tab, {sno: 'TEXT', state: 'TEXT', districts: 'INT', selected: 'INT'},
+  return esql.setupTable(tab, {sno: 'TEXT', state: 'TEXT', districts: 'INT', selected: 'INT'},
     require('./corpus').values(), Object.assign({pk: 'sno', index: true, tsvector: {sno: 'A', state: 'B'}}, opt));
-};
+}
 
-function load() {
-  if(ready) return true;
-  loadCorpus(); setupIndex();
-  return ready = true;
-};
 
 function samplingUnits(txt) {
-  if(index==null) return [];
-  var z = [], txt = txt.replace(/\W/g, ' ');
-  var mats = index.search(txt), max = 0;
-  for(var mat of mats)
-    max = Math.max(max, Object.keys(mat.matchData.metadata).length);
-  for(var mat of mats)
-    if(Object.keys(mat.matchData.metadata).length===max) z.push(corpus.get(mat.ref));
-  return z;
-};
+  if (!corpus) load();
+  var a = [], txt = txt.replace(/\W/g, ' ');
+  var ms = index.search(txt), max = 0;
+  for (var m of ms)
+    max = Math.max(max, Object.keys(m.matchData.metadata).length);
+  for (var m of ms)
+    if (Object.keys(m.matchData.metadata).length===max) a.push(corpus.get(m.ref));
+  return a;
+}
+samplingUnits.load = load;
 samplingUnits.csv = csv;
 samplingUnits.sql = sql;
-samplingUnits.load = load;
-samplingUnits.corpus = corpus;
 module.exports = samplingUnits;
