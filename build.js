d0d7619da12d250a8178ea9c22e68a211f064066
Subhajit Sahu <wolfram77@gmail.com>
2021-03-12 16:06:30 +0530

    :ringed_planet: modernize build, index

--- build.js
+++ build.js
@@ -2,9 +2,15 @@ const parse = require('csv-parse');
 const fs = require('fs');
 const os = require('os');
 
-
-// global variables
 var asset = new Map();
+
+
+
+
+function writeFile(pth, d) {
+  d = d.replace(/\r?\n/g, os.EOL);
+  fs.writeFileSync(pth, d);
+}
 
 
 function readAsset() {
@@ -12,59 +18,65 @@ function readAsset() {
     var stream = fs.createReadStream('asset.csv').pipe(parse({columns: true, comment: '#'}));
     stream.on('data', r => {
       var {code, parents} = r;
-      parents = parents? new Set(parents.split(' ')):new Set();
+      parents = parents? new Set(parents.split(' ')) : new Set();
       asset.set(code, {parents});
     });
     stream.on('end', () => fres(asset));
   });
-};
+}
 
-function getAncestry(cod, z=new Set()) {
+
+function getAncestry(cod, a=new Set()) {
   var par = asset.get(cod).parents;
-  if(par.size===0) return z;
-  for(p of par)
-    getAncestry(p, z.add(p));
-  return z;
-};
+  if (par.size===0) return a;
+  for (p of par)
+    getAncestry(p, a.add(p));
+  return a;
+}
+
 function getChildren(cod) {
-  var z = new Set();
-  for(var [k, v] of asset)
-    if(v.parents.has(cod)) z.add(k);
-  return z;
-};
+  var a = new Set();
+  for (var [k, v] of asset)
+    if (v.parents.has(cod)) a.add(k);
+  return a;
+}
+
 
 function updateAsset() {
-  var z = new Map();
-  for(var [code, r] of asset) {
+  var a = new Map();
+  for (var [code, r] of asset) {
     var {parents} = r;
     parents = Array.from(parents).join(' ');
     var ancestry = Array.from(getAncestry(code)).join(' ');
     var children = Array.from(getChildren(code)).join(' ');
-    z.set(code, {parents, ancestry, children});
+    a.set(code, {parents, ancestry, children});
   }
-  asset = z;
-};
+  asset = a;
+}
+
 
 function writeIndex() {
-  var z = `code,parents,ancestry,children${os.EOL}`;
-  for(var [code, r] of asset)
-    z += `${code},${r.parents},${r.ancestry},${r.children}${os.EOL}`;
-  fs.writeFileSync('index.csv', z);
-};
+  var a = `code,parents,ancestry,children\n`;
+  for (var [code, r] of asset)
+    a += `${code},${r.parents},${r.ancestry},${r.children}\n`;
+  writeFile('index.csv', a);
+}
+
 
 function writeCorpus() {
-  var z = `var CORPUS = new Map([${os.EOL}`;
-  for(var [code, r] of asset)
-    z += `  ["${code}", ${JSON.stringify(r).replace(/\"(\w+)\":/g, '$1:')}],${os.EOL}`;
-  z += `]);${os.EOL}`;
-  z += `module.exports = CORPUS;${os.EOL}`;
-  fs.writeFileSync('corpus.js', z);
-};
+  var a = `var CORPUS = new Map([\n`;
+  for (var [code, r] of asset)
+    a += `  ["${code}", ${JSON.stringify(r).replace(/\"(\w+)\":/g, '$1:')}],\n`;
+  a += `]);\n`;
+  a += `module.exports = CORPUS;\n`;
+  writeFile('corpus.js', a);
+}
+
 
 async function main() {
   await readAsset();
   updateAsset();
   writeIndex();
   writeCorpus();
-};
+}
 main();
--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-const columns = require('@ifct2017/columns');
-const Sql = require('sql-extra');
 const path = require('path');
+const esql = require('sql-extra');
+const columns = require('@ifct2017/columns');
 
-var corpus = new Map();
-var ready = false;
+var corpus = null;
 
 
-function loadCorpus() {
-  for(var [k, v] of require('./corpus'))
-    corpus.set(k, v);
-};
+
+
+function load() {
+  if (corpus) return corpus;
+  corpus = require('./corpus');
+  columns.load();
+  return corpus;
+}
+
 
 function csv() {
   return path.join(__dirname, 'index.csv');
-};
+}
+
 
 function sql(tab='hierarchy', opt={}) {
-  var vals = new Set(require('./corpus').values());
+  var vals = new Set(load().values());
   vals.delete(null);
-  return Sql.setupTable(tab, {code: 'TEXT', parents: 'TEXT', ancestry: 'TEXT', children: 'TEXT'}, vals,
+  return esql.setupTable(tab, {code: 'TEXT', parents: 'TEXT', ancestry: 'TEXT', children: 'TEXT'}, vals,
     Object.assign({pk: 'code', index: true, tsvector: {code: 'A', parents: 'B', ancestry: 'C', children: 'B'}}, opt));
-};
+}
 
-function load() {
-  if(ready) return true;
-  columns.load(); loadCorpus();
-  return ready = true;
-};
 
 function hierarchy(txt) {
-  if(!ready) return null;
-  var cs = columns(txt);
-  return cs.length>0? corpus.get(cs[0].code):null;
-};
+  if (!corpus) load();
+  return columns(txt).map(r => corpus.get(r.code));
+}
+hierarchy.load = load;
 hierarchy.csv = csv;
 hierarchy.sql = sql;
-hierarchy.load = load;
-hierarchy.corpus = corpus;
 module.exports = hierarchy;
