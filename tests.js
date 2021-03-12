b5047c73d6f6c76fb7b6148022b8aa10495fe99c
Subhajit Sahu <wolfram77@gmail.com>
2021-03-12 13:29:27 +0530

    :ringed_planet: w/ tests

---
+++ tests.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const codes = require('./');
+
+
+
+
+async function testAll() {
+  await codes.load();
+
+  var a = codes('mango green');
+  var b = codes('Raw mango');
+  var c = [{name: 'Mango, green, raw (Common)', code: 'D057'}];
+  assert.deepStrictEqual(a, c);
+  assert.deepStrictEqual(b, c);
+
+  var a = codes('what is food code of atta?');
+  var b = codes('atta code');
+  var c = [
+    {name: 'Atta (H., P.)', code: 'A019'},
+    {name: 'Gahama atta (O.)', code: 'A019'},
+    {name: 'Wheat flour, atta (Common)', code: 'A019'}
+  ];
+  assert.deepStrictEqual(a, c);
+  assert.deepStrictEqual(b, c);
+}
+testAll();
