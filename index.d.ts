5d2e334bc63c9df4a4d2ee72f1ee0699f1179e43
Subhajit Sahu <wolfram77@gmail.com>
2021-03-15 13:54:52 +0530

    :microbe: w/ jsdoc

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+export interface Representation {
+  /** Type of physical quantity. */
+  type: string,
+  /** Multiplication Factor. */
+  factor: number,
+  /** Unit symbol. */
+  unit: string;
+}
+
+
+/**
+ * Loads corpus to enable queries.
+ * [📦](https://www.npmjs.com/package/@ifct2017/representations)
+ * @returns corpus {code ⇒ {type, factor, unit}}
+ */
+export function load(): Map<string, Representation>;
+
+
+/**
+ * Generates PostgreSQL statements for creating table w/ data.
+ * [📦](https://www.npmjs.com/package/@ifct2017/representations)
+ * @returns CREATE TABLE, INSERT, CREATE VIEW, CREATE INDEX statements
+ */
+ export function sql(tab: string='representations', opt: object={}): string;
+
+
+/**
+ * Gives path of CSV data file.
+ * [📦](https://www.npmjs.com/package/@ifct2017/representations)
+ * @returns .../index.csv
+ */
+ export function csv(): string;
+
+
+/**
+ * Finds matching representations of a column:code/name/tags query.
+ * [📦](https://www.npmjs.com/package/@ifct2017/representations)
+ * @param txt column:code/name/tags query
+ * @returns found ⇒ {type, factor, unit}, else null
+ * @example
+ * ```javascript
+ * representations('his');
+ * representations('Histidine');
+ * // { type: 'mass', factor: 1, unit: 'g' }
+ *
+ * representations('representation of vitamin d?');
+ * representations('what is unit of cholecalciferol?');
+ * // { type: 'mass', factor: 1000000, unit: 'ug' }
+ * ```
+ */
+function representations(txt: string): Representation | null;
+export = representations;
