2a98f1b302056694f244fde2dfbaee569420c799
Subhajit Sahu <wolfram77@gmail.com>
2021-03-15 15:09:01 +0530

    :microbe: w/ jsdoc

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
 export interface Hierarchy {
-  /** Column Code. */
-  code: string,
   /** List of Parent columns. */
   parents: string,
   /** List of Ancestor columns. */
@@ -12,47 +10,46 @@ export interface Hierarchy {
 
 /**
  * Loads corpus to enable queries.
- * [📦](https://www.npmjs.com/package/@ifct2017/contents)
- * @returns corpus {sno ⇒ {sno, title, pagenos}}
+ * [📦](https://www.npmjs.com/package/@ifct2017/hierarchy)
+ * @returns corpus {code ⇒ {parents, ancestry, children}}
  */
 export function load(): Map<string, Hierarchy>;
 
 
 /**
  * Generates PostgreSQL statements for creating table w/ data.
- * [📦](https://www.npmjs.com/package/@ifct2017/contents)
+ * [📦](https://www.npmjs.com/package/@ifct2017/hierarchy)
  * @returns CREATE TABLE, INSERT, CREATE VIEW, CREATE INDEX statements
  */
- export function sql(tab: string='contents', opt: object={}): string;
+ export function sql(tab: string='hierarchy', opt: object={}): string;
 
 
 /**
  * Gives path of CSV data file.
- * [📦](https://www.npmjs.com/package/@ifct2017/contents)
+ * [📦](https://www.npmjs.com/package/@ifct2017/hierarchy)
  * @returns .../index.csv
  */
  export function csv(): string;
 
 
 /**
- * Finds matching contents of an sno/title/pagenos query.
- * [📦](https://www.npmjs.com/package/@ifct2017/contents)
- * @param txt sno/title/pagenos query
- * @returns matches [{sno, title, pagenos}]
+ * Finds matching hierarchy of a column:code/name/tags query.
+ * [📦](https://www.npmjs.com/package/@ifct2017/hierarchy)
+ * @param txt column:code/name/tags query
+ * @returns found ⇒ {parents, ancestry, children}, else null
  * @example
  * ```javascript
- * contents('table 2');
- * contents('Water soluble vitamins');
- * // [ { sno: '6.2.',
- * //     title: 'Table 2:  Water Soluble Vitamins',
- * //     pagenos: '31' } ]
+ * hierarchy('soluble oxalic acid');
+ * hierarchy('Soluble Oxalic Acid');
+ * // { parents: 'oxalt', ancestry: 'oxalt orgac', children: '' }
  *
- * contents('what is page number of table 3?');
- * contents('fat soluble vitamin page number');
- * // [ { sno: '6.3.',
- * //     title: 'Table 3:  Fat Soluble Vitamins',
- * //     pagenos: '61' } ]
+ * hierarchy('what is hierarchy of total saturated fat?');
+ * hierarchy('who are children of total saturated fat?');
+ * // { parents: 'fatce',
+ * //   ancestry: 'fatce',
+ * //   children:
+ * //    'f4d0 f6d0 f8d0 f10d0 f11d0 f12d0 f14d0 f15d0 f16d0 f18d0 f20d0 f22d0 f24d0' }
  * ```
  */
-function contents(txt: string): [Hierarchy];
-export = contents;
+function hierarchy(txt: string): Hierarchy | null;
+export = hierarchy;
