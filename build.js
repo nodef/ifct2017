ad38c2a093667cf496f786d55b70379ae11b16fc
Subhajit Sahu <wolfram77@gmail.com>
2022-09-17 12:40:44 +0530

    :mango: update build setup

--- build.js
+++ build.js
@@ -1,82 +1,117 @@
-const parse = require('csv-parse');
-const fs = require('fs');
-const os = require('os');
+const fs    = require('extra-fs');
+const build = require('extra-build');
+const csv   = require('csv-parse');
 
-var asset = new Map();
+const owner = 'ifct2017';
+const repo  = build.readMetadata('.').name.replace(/@.+\//g, '');
 
 
-
-
-function writeFile(pth, d) {
-  d = d.replace(/\r?\n/g, os.EOL);
-  fs.writeFileSync(pth, d);
-}
 
 
 function readAsset() {
-  return new Promise((fres) => {
-    var stream = fs.createReadStream('asset.csv').pipe(parse({columns: true, comment: '#'}));
+  return new Promise(resolve => {
+    var asset  = new Map();
+    var stream = fs.createReadStream('asset.csv').pipe(csv.parse({columns: true, comment: '#'}));
     stream.on('data', r => {
       var {code, parents} = r;
       parents = parents? new Set(parents.split(' ')) : new Set();
       asset.set(code, {parents});
     });
-    stream.on('end', () => fres(asset));
+    stream.on('end', () => resolve(asset));
   });
 }
 
 
-function getAncestry(cod, a=new Set()) {
-  var par = asset.get(cod).parents;
+function getAncestry(asset, code, a=new Set()) {
+  var par = asset.get(code).parents;
   if (par.size===0) return a;
   for (p of par)
-    getAncestry(p, a.add(p));
+    getAncestry(asset, p, a.add(p));
   return a;
 }
 
-function getChildren(cod) {
+function getChildren(asset, code) {
   var a = new Set();
   for (var [k, v] of asset)
-    if (v.parents.has(cod)) a.add(k);
+    if (v.parents.has(code)) a.add(k);
   return a;
 }
 
 
-function updateAsset() {
+function updateAsset(asset) {
   var a = new Map();
   for (var [code, r] of asset) {
     var {parents} = r;
     var parents  = Array.from(parents).join(' ');
-    var ancestry = Array.from(getAncestry(code)).join(' ');
-    var children = Array.from(getChildren(code)).join(' ');
+    var ancestry = Array.from(getAncestry(asset, code)).join(' ');
+    var children = Array.from(getChildren(asset, code)).join(' ');
     a.set(code, {parents, ancestry, children});
   }
-  asset = a;
+  return a;
 }
 
 
-function writeIndex() {
+function writeIndex(asset) {
   var a = `code,parents,ancestry,children\n`;
   for (var [code, r] of asset)
     a += `${code},${r.parents},${r.ancestry},${r.children}\n`;
-  writeFile('index.csv', a);
+  fs.writeFileTextSync('index.csv', a);
 }
 
 
-function writeCorpus() {
+function writeCorpus(asset) {
   var a = `var CORPUS = new Map([\n`;
   for (var [code, r] of asset)
     a += `  ["${code}", ${JSON.stringify(r).replace(/\"(\w+)\":/g, '$1:')}],\n`;
   a += `]);\n`;
   a += `module.exports = CORPUS;\n`;
-  writeFile('corpus.js', a);
+  fs.writeFileTextSync('corpus.js', a);
 }
 
 
-async function main() {
-  await readAsset();
-  updateAsset();
-  writeIndex();
-  writeCorpus();
+async function writeIndexAndCorpus() {
+  var asset = await readAsset();
+  var asset = updateAsset(asset);
+  writeIndex(asset);
+  writeCorpus(asset);
 }
-main();
+
+
+
+
+// Publish a root package to NPM, GitHub.
+function publishRootPackage(ver) {
+  var _package = build.readDocument('package.json');
+  var m = build.readMetadata('.');
+  m.version = ver;
+  build.writeMetadata('.', m);
+  build.publish('.');
+  try { build.publishGithub('.', owner); }
+  catch {}
+  build.writeDocument(_package);
+}
+
+
+// Publish root, sub packages to NPM, GitHub.
+async function publishPackages() {
+  var m   = build.readMetadata('.');
+  var ver = build.nextUnpublishedVersion(m.name, m.version);
+  await writeIndexAndCorpus();
+  publishRootPackage(ver);
+}
+
+
+// Publish docs.
+function publishDocs() {
+  var m = build.readMetadata('.');
+  build.updateGithubRepoDetails({owner, repo, topics: m.keywords});
+}
+
+
+// Finally.
+async function main(a) {
+  if (a[2]==='publish-docs') publishDocs();
+  else if (a[2]==='publish-packages') await publishPackages();
+  else await writeIndexAndCorpus();
+}
+main(process.argv);
