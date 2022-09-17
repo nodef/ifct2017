04a2067f564b5b6dc24f45b9402c095b1047a4b4
Subhajit Sahu <wolfram77@gmail.com>
2022-09-17 10:44:15 +0530

    :mango: update build setup

--- build.js
+++ build.js
@@ -1,30 +1,70 @@
-const parse = require('csv-parse');
-const fs = require('fs');
-const os = require('os');
+const fs    = require('extra-fs');
+const build = require('extra-build');
+const csv   = require('csv-parse');
 
-var map    = new Map();
-var stream = fs.createReadStream('index.csv').pipe(parse({columns: true, comment: '#'}));
-
-
+const owner = 'ifct2017';
+const repo  = build.readMetadata('.').name.replace(/@.+\//g, '');
 
 
-function writeFile(pth, d) {
-  d = d.replace(/\r?\n/g, os.EOL);
-  fs.writeFileSync(pth, d);
+
+
+function writeCorpus() {
+  return new Promise(resolve => {
+    var map    = new Map();
+    var stream = fs.createReadStream('index.csv').pipe(csv.parse({columns: true, comment: '#'}));
+    stream.on('data', r => {
+      for (var k in r)
+        if (k!=='code') r[k] = parseFloat(r[k]);
+      map.set(r.code, r);
+    });
+    stream.on('end', () => {
+      var a = `const CORPUS = new Map([\n`;
+      for (var [k, v] of map)
+        a += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:').replace(/null/g, 'NaN')}],\n`;
+      a += `]);\n`;
+      a += `module.exports = CORPUS;\n`;
+      fs.writeFileTextSync('corpus.js', a);
+      resolve();
+    });
+  });
 }
 
 
-stream.on('data', r => {
-  for (var k in r)
-    if (k!=='code') r[k] = parseFloat(r[k]);
-  map.set(r.code, r);
-});
 
-stream.on('end', () => {
-  var a = `const CORPUS = new Map([\n`;
-  for (var [k, v] of map)
-    a += `  ["${k}", ${JSON.stringify(v).replace(/\"(\w+)\":/g, '$1:').replace(/null/g, 'NaN')}],\n`;
-  a += `]);\n`;
-  a += `module.exports = CORPUS;\n`;
-  writeFile('corpus.js', a);
-});
+
+// Publish a root package to NPM, GitHub.
+function publishRootPackage(ver) {
+  var _package = build.readDocument('package.json');
+  var m = build.readMetadata('.');
+  m.version = ver;
+  build.writeMetadata('.', m);
+  build.publish('.');
+  try { build.publishGithub('.', owner); }
+  catch {}
+  build.writeDocument(_package);
+}
+
+
+// Publish root, sub packages to NPM, GitHub.
+async function publishPackages() {
+  var m   = build.readMetadata('.');
+  var ver = build.nextUnpublishedVersion(m.name, m.version);
+  await writeCorpus();
+  publishRootPackage(ver);
+}
+
+
+// Publish docs.
+function publishDocs() {
+  var m = build.readMetadata('.');
+  build.updateGithubRepoDetails({owner, repo, topics: m.keywords});
+}
+
+
+// Finally.
+async function main(a) {
+  if (a[2]==='publish-docs') publishDocs();
+  else if (a[2]==='publish-packages') await publishPackages();
+  else await writeCorpus();
+}
+main(process.argv);
